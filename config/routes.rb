eb4f695328837ad0b4dd4201084231602c5fc476
mahamsuhaiil <mahamsuhail333@gmail.com>
2023-08-22 09:10:22 +0500

    new rails app

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  resources :attachment do    # /articles, /articles/1
+  end
+end
---
+++ db/migrate/20230822040233_create_attachment_table.rb
@@ -0,0 +1,8 @@
+class CreateAttachmentTable < ActiveRecord::Migration[7.0]
+  def change
+    create_table :attachment_tables do |t|
+
+      t.timestamps
+    end
+  end
+end
